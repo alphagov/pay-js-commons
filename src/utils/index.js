e19a227443aa849d540bb75df4070b7ae37d9c72
Jon Heslop <jon@jonheslop.com>
2019-01-10 10:26:34 +0000

    Export new utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,5 @@
 
 module.exports.isValidEmail = require('./is-valid-email')
 module.exports.fieldValidationChecks = require('./field-validation-checks')
+module.exports.countries = require('./countries')
+module.exports.currencyFormatter = require('./currency-formatter')
