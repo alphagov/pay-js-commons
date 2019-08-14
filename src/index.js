5cf65e96ec49062e40de7e3e34c4466d199901ff
Alex Bishop <alexbishop1@users.noreply.github.com>
2019-08-14 18:39:06 +0100

    PP-5542 Initial structured logging keys for Node.js applications
    A file of string constants to be used as structured logging
    keys in an attempt to encourage consistency.
    
    The keys are identical to those introduced for the Java
    applications in pay-java-commons commit
    37ca42954cfa19a0649a9df7e0b3b07695ae5fa9.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 const browsered = require('./browsered')
 const utils = require('./utils')
 const nunjucksFilters = require('./nunjucks-filters')
+const loggingKeys = require('./logging-keys')
 
 // Add to window.GOVUKPAY if in browser context
 if (typeof window !== 'undefined') {
@@ -10,4 +11,4 @@ if (typeof window !== 'undefined') {
   window.GOVUKPAY = { browsered }
 }
 
-module.exports = { browsered, utils, nunjucksFilters }
+module.exports = { browsered, utils, nunjucksFilters, loggingKeys }
