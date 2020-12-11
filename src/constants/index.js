d33587cc60f0f37669018ecff2d9093d20a18e40
Iqbal Ahmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2020-12-11 14:38:33 +0000

    PP-7360 Add Medata Column constants
    - Add constants for metadata column constants

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/*
+ Metadata Column constants take from:
+ https://github.com/alphagov/pay-java-commons/blob/master/model/src/main/java/uk/gov/pay/commons/model/charge/ExternalMetadata.java
+ */
+module.exports = {
+  externalMetadata: {
+    MAX_KEY_VALUE_PAIRS = 10;
+    MIN_KEY_LENGTH = 1;
+    MAX_KEY_LENGTH = 30;
+    MAX_VALUE_LENGTH = 100;
+  } 
+}
