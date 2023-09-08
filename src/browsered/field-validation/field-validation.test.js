8fc1fac084c5c344a28f1bad1281c91fb190afc1
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2023-09-08 09:25:25 +0100

    O’Connell

--- src/browsered/field-validation/field-validation.test.js
+++ src/browsered/field-validation/field-validation.test.js
@@ -441,7 +441,7 @@ describe('Field validation', () => {
 
     describe('where value contains NAXSI flaggable characters', () => {
       before('Act', () => {
-        document.querySelector(`#${fixtures.inputId}`).value = 'O\'Connell '
+        document.querySelector(`#${fixtures.inputId}`).value = 'O’Connell'
         document.querySelector('button').click()
       })
 
